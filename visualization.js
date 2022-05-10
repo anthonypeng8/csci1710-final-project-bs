c54950b90b344deea545f657517ee9d3a7db09b5
Edward Yan <eyanje@gmail.com>
2022-05-09 22:23:48 -0400

    Liveshare session
    Co-authored-by: anthonypeng8 <anthonypeng8@users.noreply.github.com>
    Co-authored-by: liuser23 <liuser23@users.noreply.github.com>

--- visualization.js
+++ visualization.js
@@ -1,204 +1,203 @@
-const d3 = require('d3')
+const d3 = require("d3");
 d3.selectAll("svg > *").remove();
 
-function printCard(card, xoffset, yoffset) {    
-    d3.select(svg)
-    .append('rect')
-    .attr('x', xoffset)
-    .attr('y', yoffset + 5)
-    .attr('width', 25)
-    .attr('height', 50)
-    .attr('stroke-width', 2)
-    .attr('stroke', 'black')
-    .attr('fill', 'transparent')
+
+div.innerHTML = '';
+const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+svg.style.height = `${(Math.ceil(State.atoms().length / 2 + 1)) * 250}px`;
+div.style.maxHeight = '100vh';
+div.style.overflow = 'auto';
+div.appendChild(svg);
+
+
+function printCard(card, xOffset, yOffset) {
+  d3.select(svg)
+    .append("rect")
+    .attr("x", xOffset)
+    .attr("y", yOffset + 5)
+    .attr("width", 25)
+    .attr("height", 50)
+    .attr("stroke-width", 1)
+    .attr("stroke", "black")
+    .attr("fill", "white")
     .text(card.value);
+  // Number
+  var c = Card.atom(card._atoms[0]._id);
+  d3.select(svg)
+    .append("text")
+    .attr("x", xOffset + 8)
+    .attr("y", yOffset + 25)
+    .text(c.value.toString());
+  // Suit (either simply add text of suit, or more complicated add an image for suit)
+  // h: heart
+  // s: spades
+  // c: club
+  // d: diamond
+  d3.select(svg)
+    .append("text")
+    .style("font", "14px times")
+    .attr("x", xOffset + 8.5)
+    .attr("y", yOffset + 45)
+    .text(c.suit.toString()[0]);
 
-    var c = Card.atom(card._atoms[0]._id)
-    d3.select(svg)
-        .append("text")
-        .attr("x", xoffset + 10)
-        .attr("y", yoffset + 25)
-        .text(c.value.toString());
+}
+const cToVal = (c) => parseInt(c._atoms[0].value._id);
+const cToSuit = (c) => c._atoms[0].suit._id;
 
-    d3.select(svg)
-        .append("text")
-        .style("font", "14px times")
-        .attr("x", xoffset + 10)
-        .attr("y", yoffset + 45)
-        .text(c.suit.toString()[0]);
+const sortHand = (h) =>
+  h.sort((a, b) => {
+    var n = cToVal(a) - cToVal(b);
+    if (n !== 0) {
+      return n;
+    }
+    console.log(cToSuit(a));
+    console.log(cToSuit(b));
+    return cToSuit(a).localeCompare(cToSuit(b));
+  });
 
-    // Print rectangle
-    // Number
-    // Suit (either simply add text of suit, or more complicated add an image for suit)
-        // h: heart
-        // s: spades
-        // c: club
-        // d: diamond
+function printPile(stateAtom, player, xOffset, yOffset) {
+  var curr_xOffset = xOffset + 10;
+  sortHand(player.hand[stateAtom]._tuples).forEach((card) => {
+    printCard(card, curr_xOffset, yOffset);
+    curr_xOffset = curr_xOffset + 30;
+  });
 }
 
-function printPile(stateAtom, player, xoffset, yoffset) {
-    // printcard() based on how many n cards in deck
-    
-    // for (n = 0; n <= 5; n++) {
-    //     // printCard(n, yoffset, )
-    //     if(State.atom("State"+b) != null)
-    //       printState(State.atom("State"+b), offset)  
-    // }
+function printPlayer(stateAtom, player, xOffset, yOffset, bsCalled) {
+  const playerTurn = stateAtom.whoseTurn.equals(player);
 
-
-    // for (const card of player.hand[stateAtom]) {
-    //   printCard(card, curr_xoffset, yoffset);
-    //   curr_xoffset = curr_xoffset + 5
-    // }
-
-    //console.log(player.hand[stateAtom]._tuples)
-
-    var curr_xoffset = xoffset
-    player.hand[stateAtom]._tuples.forEach(card => {
-      printCard(card, curr_xoffset, yoffset);
-      curr_xoffset = curr_xoffset + 30
-    });
-}
-
-function printPlayer(stateAtom, player, yoffset) {
-    // hand of cards
+  // hand of cards
+  if (playerTurn) {
     d3.select(svg)
       .append("text")
-      .attr("x", 10)
-      .attr("y", yoffset + 25)
+      .attr("x", xOffset + 10)
+      .attr("y", yOffset + 25)
+      .attr("font-weight", "bolder")
+      .attr("fill", "red")
+      .text("Player " + player.toString()[1] + "\u2190");
+
+    if (bsCalled) {
+      d3.select(svg)
+        .append("text")
+        .attr("x", xOffset + 90)
+        .attr("y", yOffset + 25)
+        .attr("fill", "orange")
+        .text("Called BS");
+    }
+  } else {
+    d3.select(svg)
+      .append("text")
+      .attr("x", xOffset + 10)
+      .attr("y", yOffset + 25)
+      .attr("font-weight", "bolder")
       .text("Player " + player.toString()[1]);
-    printPile(stateAtom, player, 10, yoffset + 25)
+  }
 
-    // set of cards said played
-    // printPile()    
-
-    // bs/fold indicator?
+  printPile(stateAtom, player, xOffset, yOffset + 25);
 }
 
-function printState(stateAtom, yoffset) {
-    // Print out the 2 players
-        // hand of cards
-        // set of cards said played
-        // bs/fold indicator?
-    printPlayer(stateAtom, P1.atom("P10"), yoffset + 15)
-    printPlayer(stateAtom, P2.atom("P20"), yoffset + 90)
+function printState(stateAtom, xOffset, yOffset) {
+  // Print out the 2 players
+  const minWidth = 400;
 
-    // Current pile
+  d3.select(svg)
+    .append("rect")
+    .attr("x", xOffset)
+    .attr("y", yOffset)
+    .attr("width", 400)
+    .attr("height", 260)
+    .attr("fill", tableColor)
+    .attr("stroke-width", 2)
+    .attr("stroke", "black");
+
+  const firstState = stateAtom.equals(State.atom("State0"));
+  const emptyPile = Pile.atom("Pile0").hand[stateAtom].empty();
+
+  const bsCalled = emptyPile && !firstState;
+
+  printPlayer(stateAtom, P1.atom("P10"), xOffset, yOffset + 15, bsCalled);
+  printPlayer(stateAtom, P2.atom("P20"), xOffset, yOffset + 90, bsCalled);
+
+  // Current pile
+  if (emptyPile) {
     d3.select(svg)
       .append("text")
-      .attr("x", 10)
-      .attr("y", yoffset + 190)
-      .text("Pile");
-    var curr_xoffset = 10
-    Pile.atom("Pile0").hand[stateAtom]._tuples.forEach(card => {
-      printCard(card, curr_xoffset, yoffset + 190);
-      curr_xoffset = curr_xoffset + 30
-    });
-    // printPile(20, yoffset)
-
-    // Current pile value
+      .attr("x", xOffset + 10)
+      .attr("y", yOffset + 190)
+      .attr("font-weight", "bolder")
+      .text("Empty Pile");
+  } else {
     d3.select(svg)
-        .append("text")
-        .attr("x", 10)
-        .attr("y", yoffset + 20)
-        .text("Current Rank: " + stateAtom.currValue.toString());
+      .append("text")
+      .attr("x", xOffset + 10)
+      .attr("y", yOffset + 190)
+      .attr("font-weight", "bolder")
+      .text("Pile");
+  }
+
+  var curr_xOffset = xOffset + 10;
+
+  if (!emptyPile) {
+    sortHand(Pile.atom("Pile0").hand[stateAtom]._tuples).forEach((card) => {
+      printCard(card, curr_xOffset, yOffset + 190);
+      curr_xOffset = curr_xOffset + 30;
+    });
+  }
+
+  // Current Value
+  d3.select(svg)
+    .append("text")
+    .attr("x", xOffset + 10)
+    .attr("y", yOffset + 20)
+    .attr("font-weight", "bold")
+    .text("Current Value: " + stateAtom.currValue.toString());
+
+  // Cards Moved
+  d3.select(svg)
+    .append("text")
+    .attr("x", xOffset + 280)
+    .attr("y", yOffset + 190)
+    .attr("font-weight", "bolder")
+    .text("Cards Moved");
+
+  // Print cards moved
+  // Is this necessary? Adds clutter + can be confusing since cards moved also includes bs calls (i.e cards moved != cards played)
+  var curr_xoffset = xOffset + 280;
+  stateAtom.cardsMoved._tuples.forEach((card) => {
+    printCard(card, curr_xoffset, yOffset + 190);
+    curr_xoffset = curr_xoffset + 30;
+  });
 }
+
+const tableColor = "#458554";
+
+var responsiveDivHeight = 30 * 30;
+var legendSVG = d3
+  .select(svg)
+  .attr("height", "100%")
+  .attr("width", "100%")
+  .attr("viewBox", "0 0 850 " + responsiveDivHeight + "")
+  .attr("preserveAspectRatio", "xMinYMin");
 
 // This is for the states, in this case b = 10 or 11 states
-var offset = 0
-for(b = 0; b <= 10; b++) {  
-    if(State.atom("State"+b) != null)
-        printState(State.atom("State"+b), offset)  
-    offset = offset + 250
+var yOffset = 0;
+for (row = 0; row <= 4; row++) {
+  var xOffset = 0;
+  for (col = 0; col <= 1; col++) {
+    const stateNum = row * 2 + col;
+    const stateName = "State" + stateNum;
+    if (stateNum > 9) {
+      break;
+    }
+
+    if (State.atom(stateName) != null)
+      var stateAtom = State.atom("State" + stateNum);
+
+    printState(stateAtom, xOffset, yOffset);
+
+    xOffset += 420;
+    if (col == 1) {
+      yOffset = yOffset + 280;
+    }
+  }
 }
-
-
-// // TicTacToe
-// const d3 = require('d3')
-// d3.selectAll("svg > *").remove();
-
-// function printValue(row, col, yoffset, value) {
-//   d3.select(svg)
-//     .append("text")
-//     .style("fill", "black")
-//     .attr("x", (row+1)*10)
-//     .attr("y", (col+1)*14 + yoffset)
-//     .text(value);
-// }
-
-// function printState(stateAtom, yoffset) {
-//   for (r = 0; r <= 2; r++) {
-//     for (c = 0; c <= 2; c++) {
-//       printValue(r, c, yoffset,
-//                  stateAtom.board[r][c]
-//                  .toString().substring(0,1))  
-//     }
-//   }
-  
-//   d3.select(svg)
-//     .append('rect')
-//     .attr('x', 5)
-//     .attr('y', yoffset+1)
-//     .attr('width', 40)
-//     .attr('height', 50)
-//     .attr('stroke-width', 2)
-//     .attr('stroke', 'black')
-//     .attr('fill', 'transparent');
-// }
-
-
-// var offset = 0
-// for(b = 0; b <= 10; b++) {  
-//   if(State.atom("State"+b) != null)
-//     printState(State.atom("State"+b), offset)  
-//   offset = offset + 55
-// }
-
-
-// // Connect4
-// const d3 = require('d3')
-// d3.selectAll("svg > *").remove();
-
-// function printValue(row, col, xoffset, yoffset, value) {
-  
-//   var color = "red"
-//   if (value == "B")
-//     color = "blue"
-//   d3.select(svg)
-//     .append("text")
-//     .style("fill", color)
-//     .attr("x", (col+1)*10 + xoffset)
-//     .attr("y", (row+1.2)*14 + yoffset)
-//     .text(value);
-// }
-
-// function printState(stateAtom, xoffset, yoffset) {
-//   for (r = 0; r <= 5; r++) {
-//     for (c = 0; c <= 6; c++) {
-//       printValue(r, c, xoffset, yoffset,
-//                  stateAtom.board[r][c]
-//                  .toString().substring(0,1))  
-//     }
-//   }
-  
-//   d3.select(svg)
-//     .append('rect')
-//     .attr('x', xoffset+5)
-//     .attr('y', yoffset+1)
-//     .attr('width', 80)
-//     .attr('height', 90)
-//     .attr('stroke-width', 2)
-//     .attr('stroke', 'black')
-//     .attr('fill', 'transparent');
-// }
-
-// var col = 0
-// var offset = 0
-// for(b = 0; b <= 42; b++) {
-//   if(State.atom("State"+b) != null)
-//     printState(State.atom("State"+b), col*85 ,offset)
-//   if (col == 4) {
-//     col = 0
-//     offset = offset + 95
-//   } else {col = col + 1}  
-// }
